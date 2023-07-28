e99d0bdfd323defe1900c24cc627966a8bff2b10
CeDiBuG <cedibug@gmail.com>
2023-07-28 17:36:50 +0200

    Module Cédric v2

--- Projet1/Module_Cédric.py
+++ Projet1/Module_Cédric.py
@@ -1,81 +1,65 @@
 import requests
 
-class Runner():
+class Runner():     
     
-    def __init__ (self, nickname):
+    def __init__(self, nickname):
         
         self.nickname = nickname
-        self.name = ""
-        self.country = "" 
-        self.ville = ""
-        self.twitch = ""
-        self.youtube = ""
         
-        
-    def get_runners(self):
+    def get_runner(self):
         #Get nickname in speedrun database 
         url = "https://www.speedrun.com/api/v1/users/"
         response_API = requests.get(f'{url}{self.nickname}')
         
-        #Get required data 
-        self.name = response_API.json()['data']['names']['international']
-        self.country = response_API.json()['data']['location']['country']['names']['international']
-        self.ville = response_API.json()['data']['location']['region']['names']['international']
-        self.twitch = response_API.json()['data']['twitch']
-        self.youtube = response_API.json()['data']['youtube']['uri']
-        
-        #print all of them
-        print(f"Nom : {self.name}")
-        print(f"Pays : {self.country}")
-        print(f"Ville : {self.ville}")
-        print(f"Twitch : {self.twitch}")
-        print(f"Youtube : {self.youtube}")
+        return response_API.json()
         
         
 class SearchGame():
     
-    def __init__ (self, gamename):
-    
+    def __init__(self, gamename):
+        
         self.gamename = gamename
-        self.game_name = ""
-        self.release_date = "" 
     
     def get_game(self):
         #Get name of the game and att to the url
         url = "https://www.speedrun.com/api/v1/games/"
         response_API = requests.get(f'{url}{self.gamename}')
         
-        #Get data for API 
-        self.game_name = response_API.json()['data']['names']['international']
-        self.release_date = response_API.json()['data']['release-date'] 
-        
-        #Print data
-        print(f"Nom du jeux : {self.game_name}")
-        print(f"Date de sortie : {self.release_date}")
+        return response_API.json()  
         
 class SearchCategory():
     
-    def __init__ (self, gamename):
-    
-        self.categories = ""
-        self.selected_category = ""
-        self.gamename = ""
+    def __init__(self, gamename):
         
+        self.gamename = gamename
+        self.selected_category = ""
+           
     def get_category(self):
+        
         
         url_categories = (f"https://www.speedrun.com/api/v1/games/{self.gamename}/categories")
         categories_API = requests.get(url_categories)
-        self.categories = categories_API.json()['data']
-        for category in self.categories:
-            print(f" Categorie :  {category['id']} : {category['name']}")
+        
+        return categories_API.json()
             
-    def get_leaderboard(self, gamename):
+    def get_leaderboard(self):
         
         #if self.selected_category in self.categories:
             url_leaderboard = (f"https://www.speedrun.com/api/v1/leaderboards/{self.gamename}/category/{self.selected_category}")
             leaderboard_API = requests.get(url_leaderboard)
-            self.leaderboard = leaderboard_API.json()['data']['run']
-            for Place in self.categories:
-                print(f" Nom :  {Place['place']}")
-                        
+            
+            return leaderboard_API.json()
         
+    def set_category(self, chosen_category):
+        
+        self.selected_category = chosen_category
+        
+#print(Runner("CeDiBuG").get_runner())
+#print(SearchGame("re4steam").get_game())               
+#print(SearchCategory("re4steam").get_category()) 
+        
+#tg = SearchCategory("re4steam")
+#tg.set_category("xd17p04d")
+#print(tg.get_leaderboard())
+
+#print(SearchCategory("re4steam").get_category())     
