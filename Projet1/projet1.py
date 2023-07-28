1ac697969d373718584455f4979f098c5d496228
Vincent <panouilleres.vincent@yahoo.fr>
2023-07-28 16:14:25 +0200

    Ajout du script pour démarrer la gui

--- Projet1/projet1.py
+++ Projet1/projet1.py
@@ -3,10 +3,27 @@
 #               Anne Cadeillan  -  Cédric Artaud  -  Vincent PANOUILLERES               #
 #########################################################################################
 
+from ModuleVincent.basic_user_interface import BasicUserInterface
 
 def main():
-    print("Hello world")
-    print ("Second hello world")
+
+    ui = BasicUserInterface()
+    ui.start()
+    # Il faut que la liste des catégorie soit ajoutées sous le format
+#    {
+#        "1": ["id", "name"],
+#        "2": ["id", "name"],
+#        etc...
+#        }
+#   puis on peux appeler la méthode suivante
+#   ui.choose_category(dict)    
+
+    ui.choose_category()
+
+    print(ui.get_choice("game"))
+    print(ui.get_choice("category"))
+
+    print(ui.get_choices())
 
 if __name__ == "__main__":
     main()
