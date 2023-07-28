b37c974d417bb0d2af5f4c0e5b4777085bc84b87
Vincent <panouilleres.vincent@yahoo.fr>
2023-07-28 10:34:30 +0200

    Création du repo pour l'examen

---
+++ Projet2/projet2.py
@@ -0,0 +1,11 @@
+#########################################################################################
+#                      Projet 2 Examen Semaine Python Livecampus                        #
+#               Anne Cadeillan  -  Cédric Artaud  -  Vincent PANOUILLERES               #
+#########################################################################################
+
+
+def main():
+    print("Hello world")
+
+if __name__ == "__main__":
+    main()
