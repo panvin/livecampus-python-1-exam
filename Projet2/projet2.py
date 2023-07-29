9dc2047f36a180837cce2636aad925dc03e8bb7e
Vincent <panouilleres.vincent@yahoo.fr>
2023-07-29 22:20:07 +0200

    Correction du script du projet 2 pour ajouter les données

--- Projet2/projet2.py
+++ Projet2/projet2.py
@@ -4,10 +4,12 @@
 #########################################################################################
 
 from ModuleVincent.file_utils import FileUtils
-from ModuleCedric.erreur_report import Error_Report
+from ModuleCedric.log_report import Log_Report
+from ModuleAnne.classesDiagnostic import diagnostic
+from pprint import pprint
 import pathlib
 import os
-from pprint import pprint
+
 def main():
     
     # Définition des path utilisés dans le projet
@@ -23,23 +25,22 @@ def main():
     file_utils.simple_copy_file(json_path)
 
     #Extraction des données du fichier json en passant le path
-    #report_utils = Error_Report()
-    #logs_list = report_utils.export_json(json_path)
+    report_utils = Log_Report()
+    logs_list = report_utils.extract_logs(json_path)
 
-    #logs_dict = { "log level": [i[1] for i in logs_list],
-    #              "message"  : [i[2] for i in logs_list],
-    #             }
-    logs_dict = { "log level": ["error", "warning", "warning"],
-                  "message"  : ["Message 1","Message 2", "Message 3"],
+    # Adaptation du format de données pour l'export xlsx
+    logs_dict = { "log level": [i[1] for i in logs_list],
+                  "message"  : [i[2] for i in logs_list],
                  }
     
-    # Modèle de donnée pour Anne
-    infos_dict = {
-        "OS" : ["Windows 10 Pro"],
-        "CPU": ["Infos cpu"],
-        "RAM": ["Infos Ram"],
-    }
-
+    infos_dict= diagnostic()
+    # Adaptation du format de données pour l'export xlsx, il faut qu'on ait le mêmem nombre de lignes dans chaques listes
+    max_array = len(infos_dict["Les variables d'environnements"])
+    print(max_array)
+    for key, rows in infos_dict.items():
+        while len(rows)< max_array:
+            rows.append("")
+ 
     file_utils.export_as_xlsx(xlsx_path, logs_dict, infos_dict)
 
 if __name__ == "__main__":
