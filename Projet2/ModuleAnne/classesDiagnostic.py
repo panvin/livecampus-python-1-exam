32926c63655d9d5e9169c4ca5f6b621ff284f7cc
tank-b <anne.cadeillan@gmail.com>
2023-07-29 17:50:36 +0200

    7/9

--- Projet2/ModuleAnne/classesDiagnostic.py
+++ Projet2/ModuleAnne/classesDiagnostic.py
@@ -42,7 +42,7 @@ def diagnostic():
 
     # -----------------5 PROCESSUS LES PLUS EXIGEANTS EN TERME DE CONSOMMATION-----------------
     
-    #Créer un dictionnaire
+    #Créer un dictionnaire de processus
     processes_dict={}
 
     for proc in psutil.process_iter(['name', 'memory_percent']):
@@ -54,18 +54,49 @@ def diagnostic():
 
         #Mettre ces données dans le dictionnaire des catégories
         processes_dict.update({process_name:process_memory_percent})
+        #Classer le dictionnaire par ordre croissant
         sorted_dict = sorted(processes_dict.items(), key=lambda x:x[1], reverse = True)
         converted_dict = dict(sorted_dict)
     
     i = 0
     for k, value in converted_dict.items():
         i = i + 1
-        print(k)
-        print(value)
+        #print(k)
+        #print(value)
         if i  == 5:
             break
-            
+        
+    # -----------------LES VARIABLES D'ENVIRONNEMENT-----------------
 
+    env_variables = os.environ
+
+    #-----------------PARTITION DES DISQUES-----------------
+
+    partitions = psutil.disk_partitions()
+    for data in partitions:
+        device = data[0]
+        filesystem = data[2]
+        #-----------------ESPACE RESTANT SUR LES DISQUES-----------------
+        #Ici le TRY EXCEPT est inscrit au cas où des disques n'auraient pas les permissions appropriées, comme les lecteurs DVD
+        try:
+            disk_space = psutil.disk_usage(device)
+        except:
+            continue
+        free_space = disk_space[2]
+        #A CONVERTIR EN GO POUR PLUS DE LISIBILITE
+
+    #-----------------NOM DES INTERFACES RESEAUX-----------------
+
+
+
+    #-----------------BOOT TIME AU FORMAT HEURES:MINUTES:SECONDES-----------------
+
+
+
+
+    #def build_dict():
+        #Prend en entrée les variables conçues dans la fonction précédente
+        #La transforme en dictionnaire
 
 
 
