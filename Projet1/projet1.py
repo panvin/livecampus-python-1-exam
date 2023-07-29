d3185b39cf4b1050c3d4fcd6e55d9d41cb15fb53
Vincent <panouilleres.vincent@yahoo.fr>
2023-07-29 12:38:10 +0200

    Ajout d'éléments d'interfaces

--- Projet1/projet1.py
+++ Projet1/projet1.py
@@ -5,29 +5,54 @@
 
 from ModuleVincent.basic_user_interface import BasicUserInterface
 from ModuleCedric.Module_Cédric import * 
+from moduleAnne.classesPandas import JsonToDictionnary
+from pprint import pprint
 
 def main():
 
-#    ui = BasicUserInterface()
-#    ui.start()
-    # Il faut que la liste des catégorie soit retournées sous le format
-#    {
-#        "1": ["id", "name"],
-#        "2": ["id", "name"],
-#        etc...
-#        }
-#   puis on peux appeler la méthode suivante
-#   ui.choose_category(dict)    
+    ui = BasicUserInterface()
+    json_tool = JsonToDictionnary()
 
-#    json_game = SearchGame(ui.get_choice("game")).get_game()
-    json_game = SearchGame("j1nem5x1").get_game()
-    
-    game_category = SearchCategory("j1nem5x1")
-    json_category = game_category.get_category()
+    data = {
+        "Game": "",
+        "Release date": "",
+        "Category": "",
+        "Leaderboard": [["Position", "Player", "Time"]]
+    }
 
-    game_category.set_category("824r4wgd")
+    ui.start()
+    # Récupération des informations du jeu
+    json_game = SearchGame(ui.get_choice("game")).get_game()
+    game_infos = json_tool.export_json_games(json_game)
+    data["Game"] = game_infos["1"][0]
+    data["Release date"] = game_infos["1"][1]
+
+    # Récupération et choix des catégories
+    game_categories = SearchCategory(ui.get_choice("game"))
+    catogories_dict = json_tool.export_json_categories(game_categories.get_category())
+    ui.set_categories(catogories_dict)
+
+    ui.choose_category()
+
+    print(ui.get_choice("category"))
+    game_categories.set_category(ui.get_choice("category"))
+    json_leaderboard = game_categories.get_leaderboard()
+
+    leaderboard_dict = json_tool.export_json_leaderboard(json_leaderboard)
+
+    pprint(leaderboard_dict)
+
+
+
+#    ui.choose_category(dict)    
     
-    json_leaderboard = game_category.get_leaderboard() 
+    
+#    game_category = SearchCategory("j1nem5x1")
+#    json_category = game_category.get_category()
+
+#    game_category.set_category("824r4wgd")
+    
+#    json_leaderboard = game_category.get_leaderboard() 
 
 #    print(ui.get_choice("game"))
 #    print(ui.get_choice("category"))
