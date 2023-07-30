d2d46fee51213e76b04bc66693ccc7fe8e0f89e6
Vincent <panouilleres.vincent@yahoo.fr>
2023-07-30 18:44:51 +0200

    Ajout de la gestion d'erreur sur le path du fichier json

--- Projet2/projet2.py
+++ Projet2/projet2.py
@@ -24,17 +24,25 @@ def main():
     file_utils = FileUtils()
 
     #Copie du fichier json
-    file_utils.simple_copy_file(json_path)
-
-    #Extraction des données du fichier json en passant le path
-    report_utils = Log_Report()
-    logs_list = report_utils.extract_logs(json_path)
-
-    # Adaptation du format de données pour l'export xlsx
-    logs_dict = { "log level": [i[1] for i in logs_list],
-                  "message"  : [i[2] for i in logs_list],
-                 }
+    if os.path.isfile(json_path):
+        file_utils.simple_copy_file(json_path)
     
+
+        #Extraction des données du fichier json en passant le path
+        report_utils = Log_Report()
+        logs_list = report_utils.extract_logs(json_path)
+
+        # Adaptation du format de données pour l'export xlsx
+        logs_dict = { "log level": [i[1] for i in logs_list],
+                    "message"  : [i[2] for i in logs_list],
+                    }
+
+    else:
+        print(f"Le fichier json {json_path} n'existe pas")
+        logs_dict = { "log level": [""],
+                    "message"  : [""],
+                    }
+
     infos_dict= Diagnostic().create_diagnostic()
 
     # Adaptation du format de données pour l'export xlsx, il faut qu'on ait le même nombre de lignes dans chaques listes
